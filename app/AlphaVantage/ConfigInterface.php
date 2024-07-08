ff9bb78ce57c48646c48c0deb7bb46390a8094e5
Ihor Pronin <iggyster@users.noreply.github.com>
2024-07-08 17:16:37 +0300

    Fix code style

--- app/AlphaVantage/ConfigInterface.php
+++ app/AlphaVantage/ConfigInterface.php
@@ -4,14 +4,13 @@ namespace App\AlphaVantage;
 
 interface ConfigInterface
 {
-    public const NAME = 'alpha_vantage';
-    public const URL = 'url';
-    public const API_KEY = 'apikey';
-    public const CACHE_TTL = 'cache_ttl';
-    public const SYMBOLS = 'symbols';
-    public const CONFIG_URL = 'services.'.self::NAME.'.'.self::URL;
-    public const CONFIG_API_KEY = 'services.'.self::NAME.'.'.self::API_KEY;
-    public const CONFIG_SYMBOLS = 'services.'.self::NAME.'.'.self::SYMBOLS;
-
-    public const CONFIG_CACHE_TTL = 'services.'.self::NAME.'.'.self::CACHE_TTL;
+    public const string NAME = 'alpha_vantage';
+    public const string URL = 'url';
+    public const string API_KEY = 'apikey';
+    public const string CACHE_TTL = 'cache_ttl';
+    public const string SYMBOLS = 'symbols';
+    public const string CONFIG_URL = 'services.'.self::NAME.'.'.self::URL;
+    public const string CONFIG_API_KEY = 'services.'.self::NAME.'.'.self::API_KEY;
+    public const string CONFIG_SYMBOLS = 'services.'.self::NAME.'.'.self::SYMBOLS;
+    public const string CONFIG_CACHE_TTL = 'services.'.self::NAME.'.'.self::CACHE_TTL;
 }
