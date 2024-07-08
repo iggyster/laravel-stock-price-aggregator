63f15fd31c2c1bb1673d48935fdd7cebbe8efba9
Ihor Pronin <iggyster@users.noreply.github.com>
2024-07-08 16:44:26 +0300

    Activate route provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AlphaVantageServiceProvider;
+use App\Providers\RouteServiceProvider;
 
 return [
     AlphaVantageServiceProvider::class,
+    RouteServiceProvider::class
 ];
