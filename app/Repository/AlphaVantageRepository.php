26d429765960bd7e7810d99ec4e3447fa7be5b73
Ihor Pronin <iggyster@users.noreply.github.com>
2024-07-10 11:23:07 +0300

    Add reports SQL & update fetchLatest
    Previous version of a fetchLatest has duplicated symbols.
    fetchReports is not so elegant but it's working.

--- app/Repository/AlphaVantageRepository.php
+++ app/Repository/AlphaVantageRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\AlphaVantage\Repository\RepositoryInterface;
 use App\Models\Stock as StockModel;
+use Illuminate\Support\Facades\DB;
 
 class AlphaVantageRepository implements RepositoryInterface
 {
@@ -11,8 +12,10 @@ class AlphaVantageRepository implements RepositoryInterface
 
     public function fetchLatest(): array
     {
-        $stocks = StockModel::orderBy('date', 'DESC')
-            ->groupBy(['id', 'name'])
+        $stocks = StockModel::whereIn(
+                'id',
+                StockModel::query()->selectRaw('max(id)')->groupBy('name')
+            )
             ->limit(self::LATEST_LIMIT)
             ->get();
 
@@ -34,6 +37,25 @@ class AlphaVantageRepository implements RepositoryInterface
 
     public function fetchReports(): array
     {
-        return [];
+        $reports = DB::table(
+            StockModel::selectRaw('
+                    name,
+                    price AS current_price,
+                    LEAD(price, 1) OVER (PARTITION BY name ORDER BY id DESC) AS previous_price,
+                    price - LEAD(price, 1) OVER (PARTITION BY name ORDER BY id DESC) AS price_difference,
+                    ROW_NUMBER() OVER (PARTITION BY name ORDER BY id DESC) AS rn
+                ')
+            )
+            ->where('rn', '=', '1')
+            ->get();
+
+        return $reports
+            ->map(fn($item) => [
+                'name' => $item->name,
+                'current_price' => $item->current_price,
+                'previous_price' => $item->previous_price,
+                'change' => $item->price_difference * 100 / $item->previous_price,
+            ])
+            ->toArray();
     }
 }
